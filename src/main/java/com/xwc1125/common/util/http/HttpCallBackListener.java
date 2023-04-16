8afbc4739e0e28ab338d8641b30d246ff4c0a2c0
xwc1125 <xwc1125@qq.com>
2023-04-16 17:53:17 +0800

    httputils: opt get or post method

--- src/main/java/com/xwc1125/common/util/http/HttpCallBackListener.java
+++ src/main/java/com/xwc1125/common/util/http/HttpCallBackListener.java
@@ -9,7 +9,14 @@ import okhttp3.Response;
  * @Copyright Copyright@2019
  */
 public interface HttpCallBackListener {
+    /**
+     * @param response
+     * @param data
+     */
     void onResponse(Response response, String data);
 
+    /**
+     * @param e
+     */
     void onError(Exception e);
 }
